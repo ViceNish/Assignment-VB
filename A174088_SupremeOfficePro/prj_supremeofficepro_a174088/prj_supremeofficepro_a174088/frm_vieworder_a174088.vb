0cc0d287b54ecc1384528968394bee661482a153
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-01-21 19:03:40 +0800

    vice

--- A174088_SupremeOfficePro/prj_supremeofficepro_a174088/prj_supremeofficepro_a174088/frm_vieworder_a174088.vb
+++ A174088_SupremeOfficePro/prj_supremeofficepro_a174088/prj_supremeofficepro_a174088/frm_vieworder_a174088.vb
@@ -37,6 +37,24 @@
         ref_cust(custID.Text)
         ref_staff(staffID.Text)
 
+        ref_details(lblOrderID.Text)
+
+        'Dim purchase As DataTable = run_sql_4_query("SELECT * FROM TBL_PURCHASE_A174088 WHERE FLD_ORDER_ID = '" & Id & "'")
+        'For Each row As DataRow In purchase.Rows
+        '    Dim product As DataTable = run_sql_4_query("SELECT * FROM TBL_PRODUCT_A174088 WHERE FLD_ORDER_ID = '" & row.Item("FLD_PRODUCT_ID") & "'")
+        '    lbl_product.Text += product.Rows(0).Item("FLD_PRODUCT_NAME") + Environment.NewLine
+        '    lbl_qty.Text += row.Item("FLD_QUANTITY") + Environment.NewLine
+        '    lbl_unit.Text += "RM" + product.Rows(0).Item("FLD_PRICE") + Environment.NewLine
+        '    lblSubTotal.Text += "RM" + row.Item("FLD_PRICE").ToString + Environment.NewLine
+        '    lbl_Total.Text = Val(lbl_Total.Text) + row.ItemArray("FLD_PRICE")
+        'Next
+
+        'lbl_product.Text = lbl_product.Text.Remove(lbl_product.Text.LastIndexOf(Environment.NewLine))
+        'lbl_qty.Text = lbl_qty.Text.Remove(lbl_qty.Text.LastIndexOf(Environment.NewLine))
+        'lbl_unit.Text = lbl_unit.Text.Remove(lbl_unit.Text.LastIndexOf(Environment.NewLine))
+        'lblSubTotal.Text = lblSubTotal.Text.Remove(lblSubTotal.Text.LastIndexOf(Environment.NewLine))
+        'lbl_Total.Text = "RM" + lbl_Total.Text
+
 
     End Sub
 
@@ -55,6 +73,30 @@
         myreader.Fill(mydatatable)
 
         staffName.Text = mydatatable.Rows(0).Item("FLD_STAFF_NAME")
+    End Sub
+    Private Sub ref_details(Id As String)
+        lbl_product.Text = ""
+        lbl_qty.Text = ""
+        lbl_unit.Text = ""
+        lblSubTotal.Text = ""
+        lbl_Total.Text = ""
+
+        Dim purchase As DataTable = run_sql_4_query("SELECT * FROM TBL_PURCHASE_A174088 WHERE FLD_ORDER_ID = '" & Id & "'")
+        For Each row As DataRow In purchase.Rows
+            Dim product As DataTable = run_sql_4_query("SELECT FLD_PRODUCT_NAME, FLD_PRICE FROM TBL_PRODUCT_A174088 WHERE FLD_ORDER_ID = '" & row.Item("FLD_PRODUCT_ID") & "'")
+            lbl_product.Text += product.Rows(0).Item("FLD_PRODUCT_NAME") + Environment.NewLine
+            lbl_qty.Text += row.Item("FLD_QUANTITY") + Environment.NewLine
+            lbl_unit.Text += "RM" + product.Rows(0).Item("FLD_PRICE") + Environment.NewLine
+            lblSubTotal.Text += "RM" + row.Item("FLD_PRICE").ToString + Environment.NewLine
+            lbl_Total.Text = Val(lbl_Total.Text) + row.ItemArray("FLD_PRICE")
+        Next
+
+        lbl_product.Text = lbl_product.Text.Remove(lbl_product.Text.LastIndexOf(Environment.NewLine))
+        lbl_qty.Text = lbl_qty.Text.Remove(lbl_qty.Text.LastIndexOf(Environment.NewLine))
+        lbl_unit.Text = lbl_unit.Text.Remove(lbl_unit.Text.LastIndexOf(Environment.NewLine))
+        lblSubTotal.Text = lblSubTotal.Text.Remove(lblSubTotal.Text.LastIndexOf(Environment.NewLine))
+        lbl_Total.Text = "RM" + lbl_Total.Text
+
     End Sub
 
     Private Sub cmb_OrderID_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmb_OrderID.SelectedIndexChanged
